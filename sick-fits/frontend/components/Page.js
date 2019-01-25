37289b82447fb304edcc4e8cbf69417c0dd5a4a3
Lee Len Formento <leebatef@gmail.com>
2019-01-25 14:31:16 -0500

    butttons

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
-import Header from '../components/Header'
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Meta from '../components/Meta'
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+background: red;
+font-size: ${props => (props.huge ? '100px' : '50px')};
+  span {
+    font-size: 100px;
+  }
+`;
+
+
 class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
-        <p> Hey, I'm the page component! </p>
+        <MyButton huge = "100"> 
+          Click me 
+          <span> 💩 </span>
+          </MyButton>
+          <Header />
+          <MyButton> 
+          Click me 
+          <span> 💩 </span>
+          </MyButton>
+
         {this.props.children}
       </div>
     )
